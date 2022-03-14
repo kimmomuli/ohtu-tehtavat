97fced86f01132b405dbf9c06d2a726171f031b0
Kimmo Muli <kimmo.muli@hotmail.com>
2022-03-14 19:42:16 +0200

    Tyhjennä metodi toimii

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -4,17 +4,12 @@ from ostos import Ostos
 class Ostoskori:
     def __init__(self):
         self.ostoskori = []
-        # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
     def tavaroita_korissa(self):
         return sum([ostos.lukumaara() for ostos in self.ostoskori])
-        # kertoo korissa olevien tavaroiden lukumäärän
-        # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
-        # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
 
     def hinta(self):
         return sum([ostos.hinta() for ostos in self.ostoskori])
-        # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
         if lisattava.nimi() in [ostos.tuotteen_nimi() for ostos in self.ostoskori]:
@@ -33,10 +28,7 @@ class Ostoskori:
                     self.ostoskori.remove(ostos)
 
     def tyhjenna(self):
-        pass
-        # tyhjentää ostoskorin
+        self.ostoskori = []
 
     def ostokset(self):
         return self.ostoskori
-        # palauttaa listan jossa on korissa olevat ostos-oliot
-        # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
